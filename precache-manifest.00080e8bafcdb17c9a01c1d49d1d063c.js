fea6ced87270868eabf87dd5d532257d962e694e
Ozan Erdal <oerdal@uw.edu>
2020-02-10 16:20:39 -0800

    Updates

---
+++ precache-manifest.00080e8bafcdb17c9a01c1d49d1d063c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63629200f862c6776285170d70075c2",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "47a3ced96911fa74a7cf",
+    "url": "/my-site/static/css/main.2c9edc0b.chunk.css"
+  },
+  {
+    "revision": "4b06d8cc2ed2469201c6",
+    "url": "/my-site/static/js/2.c8a4dc83.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-site/static/js/2.c8a4dc83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47a3ced96911fa74a7cf",
+    "url": "/my-site/static/js/main.69d61f32.chunk.js"
+  },
+  {
+    "revision": "839b4f1ffb4334eb1491",
+    "url": "/my-site/static/js/runtime-main.1b6100af.js"
+  }
+]);
