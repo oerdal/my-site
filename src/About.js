b5987f6d13c572f49063e6dc296883d433857830
Ozan Erdal <oerdal@uw.edu>
2020-02-10 16:20:12 -0800

    prep for deploy

--- src/About.js
+++ src/About.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 function renderText() {
   return(
-    <div id='about' className='d-flex flex-column align-items-center'>
+    <div id='about' className='d-flex flex-column align-items-center my-4'>
       <h2>About Me</h2>
       <p className='w-50 text-center'>I am a fourth year student at the University of Washington.
         I am studying toward a major Applied and Computational Math Sciences with a concentration in Scientific Computing and Numerical Algorithms.
         I am also completing a minor from the Information School at the UW and am particularly interested in data science.</p>
+      <p>My resume is available <a href='/Resume2020.pdf'>here</a>.</p> 
     </div>
   )
 }
--- src/Projects.js
+++ src/Projects.js
@@ -3,30 +3,44 @@ import Project from './Project';
 
 const Projects = () => {
   return (
-    <div id='projects' className='d-flex align-items-center'>
-      <Project
-        name='NBA Fantasy Trade Tool'
-        description=
-          'Tool for determining the +/- effects of trading selecting players.
-          Contains a database of all players and their season stats.
-          Written entirely in R and deployed to ShinyApps.'
-        link1='https://oerdal.shinyapps.io/final/'
-        link2='https://github.com/oerdal/info201-NBA'
-      />
-      <Project
-        name='Pi Alpha Phi Website'
-        description=
-          'Website for my Fraternity for external presentation.
-          Facilitates recruitment outreach and information provision.
-          Built using React.js, Bootstrap, and deployed to Heroku.'
-        link1='http://uwpaphi.com/'
-        link2='https://github.com/oerdal/paphi'
-      />
-      <div className='_project'>
-        
-      </div>
-      <div className='_project'>
-        
+    <div id='projects' className='d-flex flex-column align-items-center my-4'>
+      <h2>Projects</h2>
+      <div className='d-flex align-items-center flex-wrap'>
+        <Project
+          name='NBA Fantasy Trade Tool'
+          description=
+            'Tool for determining the +/- effects of trading selecting players.
+            Contains a database of all players and their season stats.
+            Written entirely in R and deployed to ShinyApps.'
+          link1='https://oerdal.shinyapps.io/final/'
+          link2='https://github.com/oerdal/info201-NBA'
+        />
+        <Project
+          name='Pi Alpha Phi Website'
+          description=
+            'Website for my Fraternity for external presentation.
+            Facilitates recruitment outreach and information provision.
+            Built using React.js, Bootstrap, and deployed to Heroku.'
+          link1='http://uwpaphi.com/'
+          link2='https://github.com/oerdal/paphi'
+        />
+        <Project
+          name='NBA Team Optimization'
+          description=
+            'Linear Programming group project for maximizing team effectiveness
+            under a given salary constraint.
+            Web scraper code and LP input code written in python.'
+          link1=''
+          link2='https://github.com/sa-tony/MATH-381-Project-1'
+        />
+        <Project
+          name="Rubik's Cube Solver"
+          description=
+            "A.I. agent for solving a simplified 3x3 Rubik's cube
+            using feature-based Q-learning. Written in python."
+          link1=''
+          link2='https://github.com/AashrayAnand/rubiks-cube-reinforcement-learning'
+        />
       </div>
     </div>
   );
